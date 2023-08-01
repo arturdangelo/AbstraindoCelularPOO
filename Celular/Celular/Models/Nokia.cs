85e2a79b3bd4dd293459ccd109fde70640144ad7
Artur D Angelo <arturdangelo@gmail.com>
2023-07-31 22:14:23 -0300

    Abstraindo Celular em POO - curso Web Dio

---
+++ Celular/Celular/Models/Nokia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celular.Models
+{
+    internal class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia...");
+        }
+    }
+}
---
+++ Celular/Celular/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using Celular.Models;
+
+namespace Celular;
+public class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Smartphone Nokia:");
+        Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "111", memoria: 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Whatsapp");
+        
+        Console.WriteLine();
+
+        Console.WriteLine("Smartphone Iphone:");
+        Smartphone iphone = new Iphone(numero: "654321", modelo: "Modelo 2", imei: "222", memoria: 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");
+
+    }
+}
